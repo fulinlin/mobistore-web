49ab62c90d5b04e6e9e2374a70448c8ab1644199
aaron <462826@qq.com>
2015-07-14 11:58:14 +0800

    添加手机端页面

---
+++ src/main/webapp/wolai-mobi/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name wolaiMobiApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the wolaiMobiApp
+ */
+angular.module('wolaiMobiApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
