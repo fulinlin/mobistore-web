2597c50ec0c25961c4be6418d6ec08f6e30406fd
aaron <462826@qq.com>
2015-08-12 09:01:31 +0800

    tt

--- src/main/java/com/wolai/platform/util/WebUtils.java
+++ src/main/java/com/wolai/platform/util/WebUtils.java
@@ -212,8 +212,7 @@ public class WebUtils {
     public static HttpServletResponse AddCorsSupport(ServletResponse response) {
     	HttpServletResponse res = (HttpServletResponse) response;
     	
-//    	res.addHeader("Access-Control-Allow-Origin", Constant.WEB_CLIENT_URL_LOCAL);
-    	res.addHeader("Access-Control-Allow-Origin", Constant.WEB_CLIENT_URL_TEST);
+    	res.addHeader("Access-Control-Allow-Origin", Constant.WEB_CLIENT_URL);
     	res.addHeader("Access-Control-Allow-Credentials", "true");
         
     	res.addHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
