1f6b3c760b9f49e6c9ea11890bf0b219af91ccf9
aaron <462826@qq.com>
2015-08-27 15:50:57 +0800

    tt

--- src/main/webapp/wolai-mobi/app/scripts/utils/constants.js
+++ src/main/webapp/wolai-mobi/app/scripts/utils/constants.js
@@ -1,7 +1,7 @@
 var client = angular.module('wolaiMobiApp');
 
 client.factory('Constant', ['$location', function($location) {
-	var ApiServer = 'http://10.0.1.100:8080/wolai-web/';
+	var ApiServer = 'http://101.200.189.57:9090/wolai-web/';
 	var ApiUrl = ApiServer + 'api/web/';
 	
     return {
