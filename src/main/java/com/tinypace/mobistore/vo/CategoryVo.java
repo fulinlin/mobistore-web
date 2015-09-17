8ed2344ceb7dc89d5b4fc5808b7ba7a6f683d97f
aaron <462826@qq.com>
2015-09-17 15:57:08 +0800

    tt

---
+++ src/main/java/com/tinypace/mobistore/vo/CategoryVo.java
@@ -0,0 +1,33 @@
+package com.tinypace.mobistore.vo;
+
+
+public class CategoryVo {
+	private String id;
+	private String code;
+	private String name;
+	private String descr;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescr() {
+		return descr;
+	}
+	public void setDescr(String descr) {
+		this.descr = descr;
+	}
+}
