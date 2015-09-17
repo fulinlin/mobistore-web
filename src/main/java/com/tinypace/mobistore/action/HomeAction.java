633892f309ac3364391ed71500fa16abff7fcda2
aaron <462826@qq.com>
2015-09-17 15:58:54 +0800

    tt

--- src/main/java/com/tinypace/mobistore/action/HomeAction.java
+++ src/main/java/com/tinypace/mobistore/action/HomeAction.java
@@ -1,5 +1,6 @@
 package com.tinypace.mobistore.action;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -16,8 +17,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.tinypace.mobistore.constant.Constant;
 import com.tinypace.mobistore.controller.BaseController;
 import com.tinypace.mobistore.entity.StrCategory;
+import com.tinypace.mobistore.entity.StrProduct;
 import com.tinypace.mobistore.service.CategoryService;
 import com.tinypace.mobistore.service.ProductService;
+import com.tinypace.mobistore.util.BeanUtilEx;
+import com.tinypace.mobistore.vo.CategoryVo;
+import com.tinypace.mobistore.vo.ProductVo;
 
 @Controller
 @RequestMapping(Constant.API + "home/")
@@ -33,10 +38,19 @@ public class HomeAction extends BaseController {
 	public Map<String, Object> index(HttpServletRequest request, @RequestBody Object json) {
 		Map<String, Object> ret = new HashMap<String, Object>();
 		
-		List<StrCategory> ls = categoryService.listAll();
+		List<StrCategory> pos = categoryService.listAll();
+		
+		List<CategoryVo> vos = new ArrayList<CategoryVo>();
+		
+		for (StrCategory po : pos) {
+			CategoryVo vo = new CategoryVo();
+			BeanUtilEx.copyProperties(vo, po);
+			
+			vos.add(vo);
+		}
 		
 		ret.put("code", 1);
-		ret.put("data", ls);
+		ret.put("data", vos);
 		return ret;
 	}
 }
